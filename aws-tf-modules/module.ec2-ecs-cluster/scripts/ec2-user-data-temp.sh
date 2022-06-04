92b9bf8c952e4514f722b356734463c0c33fd7a1
vivek22117 <vivekmishra22117@gmail.com>
2022-06-04 07:09:25 +0530

    updating AMI script for ECS cluster EC2 instances

--- aws-tf-modules/module.ec2-ecs-cluster/scripts/ec2-user-data-temp.sh
+++ aws-tf-modules/module.ec2-ecs-cluster/scripts/ec2-user-data-temp.sh
@@ -2,3 +2,9 @@
 
 echo ECS_CLUSTER=${health_monitoring_cluster} >> /etc/ecs/ecs.config
 echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
+
+# To enable IAM roles for tasks in containers with bridge and default network modes, set ECS_ENABLE_TASK_IAM_ROLE to true. See the following example:
+echo ECS_ENABLE_TASK_IAM_ROLE=true >> /etc/ecs/ecs.config
+
+# To enable IAM roles for tasks in containers with the host network mode, set ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST to true. See the following example:
+echo ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true >> /etc/ecs/ecs.config
