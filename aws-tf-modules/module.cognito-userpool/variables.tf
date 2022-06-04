fbf527235c0d8b37ca6590e33ae66d3715830b98
vivek22117 <vivekmishra22117@gmail.com>
2022-06-04 07:09:58 +0530

    adding tf variables for cognito user pool

--- aws-tf-modules/module.cognito-userpool/variables.tf
+++ aws-tf-modules/module.cognito-userpool/variables.tf
@@ -20,12 +20,12 @@ variable "component_name" {
 #    Cognito configuration variables         #
 ##############################################
 variable "user_pool_name" {
-  type = string
+  type        = string
   description = "Cognito user pool name"
 }
 
 variable "is_username_case_sensitive" {
-  type = bool
+  type        = bool
   description = "The Username Configuration. Seting `case_sesiteve` specifies whether username case sensitivity will be applied for all users in the user pool"
 }
 
@@ -39,6 +39,90 @@ variable "auto_verified_attributes" {
   description = "The attributes to be auto-verified. Possible values: email, phone_number"
   type        = list(string)
   default     = ["email"]
+}
+
+variable "mfa_configuration" {
+  description = "Set to enable multi-factor authentication. Must be one of the following values (ON, OFF, OPTIONAL)"
+  type        = string
+  default     = "OFF"
+}
+
+# software_token_mfa_configuration
+variable "software_token_mfa_configuration" {
+  description = "Configuration block for software token MFA (multifactor-auth). mfa_configuration must also be enabled for this to work"
+  type        = map(any)
+  default     = {}
+}
+
+variable "software_token_mfa_configuration_enabled" {
+  description = "If true, and if mfa_configuration is also enabled, multi-factor authentication by software TOTP generator will be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "admin_create_user_config" {
+  description = "The configuration for AdminCreateUser requests"
+  type        = map(any)
+  default     = {}
+}
+
+variable "admin_create_user_config_allow_admin_create_user_only" {
+  description = "Set to True if only the administrator is allowed to create user profiles. Set to False if users can sign themselves up via an app"
+  type        = bool
+  default     = true
+}
+
+variable "admin_create_user_config_email_message" {
+  description = "The message template for email messages. Must contain `{username}` and `{####}` placeholders, for username and temporary password, respectively"
+  type        = string
+  default     = "{username}, your verification code is `{####}`"
+}
+
+
+variable "admin_create_user_config_email_subject" {
+  description = "The subject line for email messages"
+  type        = string
+  default     = "Your verification code"
+}
+
+variable "admin_create_user_config_sms_message" {
+  description = "- The message template for SMS messages. Must contain `{username}` and `{####}` placeholders, for username and temporary password, respectively"
+  type        = string
+  default     = "Your username is {username} and temporary password is `{####}`"
+}
+
+variable "email_configuration" {
+  description = "The Email Configuration"
+  type        = map(any)
+  default = {
+    email_sending_account  = "DEVELOPER"
+    reply_to_email_address = "email@mydomain.com"
+    source_arn             = "arn:aws:ses:us-east-1:123456789012:identity/admin@doubledigit-soltuions.com"
+  }
+}
+
+variable "email_configuration_reply_to_email_address" {
+  description = "The REPLY-TO email address"
+  type        = string
+  default     = ""
+}
+
+variable "email_configuration_source_arn" {
+  description = "ARN of the SES verified email identity to to use. Required if email_sending_account is set to DEVELOPER"
+  type        = string
+  default     = ""
+}
+
+variable "email_configuration_email_sending_account" {
+  description = "Instruct Cognito to either use its built-in functional or Amazon SES to send out emails. Allowed values: `COGNITO_DEFAULT` or `DEVELOPER`"
+  type        = string
+  default     = "COGNITO_DEFAULT"
+}
+
+variable "email_configuration_from_email_address" {
+  description = "Sender’s email address or sender’s display name with their email address (e.g. `john@example.com`, `John Smith <john@example.com>` or `\"John Smith Ph.D.\" <john@example.com>)`. Escaped double quotes are required around display names that contain certain characters as specified in RFC 5322"
+  type        = string
+  default     = null
 }
 
 # password_policy
