cc338458ece592a2a87ef6ad2277ba3bd8ffa5ac
vivek22117 <vivekmishra22117@gmail.com>
2022-06-04 07:10:23 +0530

    adding userpool TF resource

--- aws-tf-modules/module.cognito-userpool/userpool.tf
+++ aws-tf-modules/module.cognito-userpool/userpool.tf
@@ -1,4 +1,26 @@
 locals {
+
+  # admin_create_user_config
+  # If no admin_create_user_config list is provided, build a admin_create_user_config using the default values
+  admin_create_user_config_default = {
+    allow_admin_create_user_only = lookup(var.admin_create_user_config, "allow_admin_create_user_only", null) == null ? var.admin_create_user_config_allow_admin_create_user_only : lookup(var.admin_create_user_config, "allow_admin_create_user_only")
+    email_message                = lookup(var.admin_create_user_config, "email_message", null) == null ? var.admin_create_user_config_email_message : lookup(var.admin_create_user_config, "email_message")
+    email_subject                = lookup(var.admin_create_user_config, "email_subject", null) == null ? var.admin_create_user_config_email_subject : lookup(var.admin_create_user_config, "email_subject")
+    sms_message                  = lookup(var.admin_create_user_config, "sms_message", null) == null ? var.admin_create_user_config_sms_message : lookup(var.admin_create_user_config, "sms_message")
+
+  }
+
+  email_configuration_default = {
+    reply_to_email_address = lookup(var.email_configuration, "reply_to_email_address", null) == null ? var.email_configuration_reply_to_email_address : lookup(var.email_configuration, "reply_to_email_address")
+    source_arn             = lookup(var.email_configuration, "source_arn", null) == null ? var.email_configuration_source_arn : lookup(var.email_configuration, "source_arn")
+    email_sending_account  = lookup(var.email_configuration, "email_sending_account", null) == null ? var.email_configuration_email_sending_account : lookup(var.email_configuration, "email_sending_account")
+    from_email_address     = lookup(var.email_configuration, "from_email_address", null) == null ? var.email_configuration_from_email_address : lookup(var.email_configuration, "from_email_address")
+  }
+
+  email_configuration = [local.email_configuration_default]
+
+  admin_create_user_config = [local.admin_create_user_config_default]
+
   password_policy_is_null = {
     minimum_length                   = var.password_policy_minimum_length
     require_lowercase                = var.password_policy_require_lowercase
@@ -18,8 +40,16 @@ locals {
 
   }
 
-  # Return the default values
   password_policy = var.password_policy == null ? [local.password_policy_is_null] : [local.password_policy_not_null]
+
+  # software_token_mfa_configuration
+  # If no software_token_mfa_configuration is provided, build a software_token_mfa_configuration using the default values
+  software_token_mfa_configuration_default = {
+    enabled = lookup(var.software_token_mfa_configuration, "enabled", null) == null ? var.software_token_mfa_configuration_enabled : lookup(var.software_token_mfa_configuration, "enabled")
+  }
+
+  software_token_mfa_configuration = var.mfa_configuration == "OFF" ? [] : [local.software_token_mfa_configuration_default]
+
 }
 
 resource "aws_cognito_user_pool" "pool" {
@@ -27,9 +57,46 @@ resource "aws_cognito_user_pool" "pool" {
   name                     = var.user_pool_name
   alias_attributes         = var.alias_attributes
   auto_verified_attributes = var.auto_verified_attributes
+  mfa_configuration        = var.mfa_configuration
 
   username_configuration {
     case_sensitive = var.is_username_case_sensitive
+  }
+
+  # admin_create_user_config
+  dynamic "admin_create_user_config" {
+    for_each = local.admin_create_user_config
+    content {
+      allow_admin_create_user_only = lookup(admin_create_user_config.value, "allow_admin_create_user_only")
+
+      dynamic "invite_message_template" {
+        for_each = lookup(admin_create_user_config.value, "email_message", null) == null && lookup(admin_create_user_config.value, "email_subject", null) == null && lookup(admin_create_user_config.value, "sms_message", null) == null ? [] : [1]
+        content {
+          email_message = lookup(admin_create_user_config.value, "email_message")
+          email_subject = lookup(admin_create_user_config.value, "email_subject")
+          sms_message   = lookup(admin_create_user_config.value, "sms_message")
+        }
+      }
+    }
+  }
+
+  # email_configuration
+  dynamic "email_configuration" {
+    for_each = local.email_configuration
+    content {
+      reply_to_email_address = lookup(email_configuration.value, "reply_to_email_address")
+      source_arn             = lookup(email_configuration.value, "source_arn")
+      email_sending_account  = lookup(email_configuration.value, "email_sending_account")
+      from_email_address     = lookup(email_configuration.value, "from_email_address")
+    }
+  }
+
+  # software_token_mfa_configuration
+  dynamic "software_token_mfa_configuration" {
+    for_each = local.software_token_mfa_configuration
+    content {
+      enabled = lookup(software_token_mfa_configuration.value, "enabled")
+    }
   }
 
 
