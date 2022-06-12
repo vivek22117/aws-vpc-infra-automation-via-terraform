b7f7cbe5b8a685d1c371459a76be96de50850553
vivek22117 <vivekmishra22117@gmail.com>
2022-06-12 22:22:22 +0530

    adding db infra pipeline

--- aws-tf-modules/module.ec2-ecs-cluster/scripts/ec2-user-data-temp.sh
+++ aws-tf-modules/module.ec2-ecs-cluster/scripts/ec2-user-data-temp.sh
@@ -8,3 +8,5 @@ echo ECS_ENABLE_TASK_IAM_ROLE=true >> /etc/ecs/ecs.config
 
 # To enable IAM roles for tasks in containers with the host network mode, set ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST to true. See the following example:
 echo ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true >> /etc/ecs/ecs.config
+
+ECS_IMAGE_PULL_BEHAVIOR=always >> /etc/ecs/ecs.config
