2e00724172749031260914c089dd0635b1af1aac
vivek22117 <vivekmishra22117@gmail.com>
2022-06-25 12:18:09 +0530

    adding TF resource for Cognito domain

--- aws-tf-modules/module.cognito-userpool/domain.tf
+++ aws-tf-modules/module.cognito-userpool/domain.tf
@@ -1,7 +1,7 @@
 resource "aws_cognito_user_pool_domain" "user_pool_domain" {
-  count = ! var.enabled || var.domain == null || var.domain == "" ? 0 : 1
+  count = !var.enabled || var.cognito_domain == null || var.cognito_domain == "" ? 0 : 1
 
-  domain = var.domain
-  //  certificate_arn = var.domain_certificate_arn
-  user_pool_id = aws_cognito_user_pool.pool.id
+  domain          = var.cognito_domain
+  certificate_arn = aws_acm_certificate.cognito_auth_acm.arn
+  user_pool_id    = aws_cognito_user_pool.pool.id
 }
