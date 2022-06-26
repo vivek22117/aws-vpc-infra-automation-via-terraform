ecd35035d1c03f0ba79a1d3f7014f6af7860c0dc
vivek22117 <vivekmishra22117@gmail.com>
2022-06-26 12:15:29 +0530

    restructuring the tf script for userpool and its domain

--- deployment/cognito-userpool/main.tf
+++ deployment/cognito-userpool/main.tf
@@ -1,19 +1,17 @@
 ####################################################
 #           VPC module deployment                  #
 ####################################################
-module "route53_config" {
-  source = "../../aws-tf-modules/module.aws-route53"
-
-  default_region = var.default_region
-  environment    = var.environment
-  route53_domain = var.route53_domain
-  cognito_domain = var.cognito_domain
-}
+//module "route53_config" {
+//  source = "../../aws-tf-modules/module.aws-route53"
+//
+//  default_region = var.default_region
+//  environment    = var.environment
+//  route53_domain = var.route53_domain
+//  cognito_domain = var.cognito_domain
+//}
 
 
 module "cognito_userpool" {
-  depends_on = [module.route53_config]
-
   source = "../../aws-tf-modules/module.cognito-userpool"
 
   default_region = var.default_region
