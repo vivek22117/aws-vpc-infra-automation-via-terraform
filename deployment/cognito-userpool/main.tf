714656107e4917c20c85800a7bc4f92b03da170f
vivek22117 <vivekmishra22117@gmail.com>
2022-06-26 09:49:19 +0530

    adding all the attributes for cognito main script deployment

--- deployment/cognito-userpool/main.tf
+++ deployment/cognito-userpool/main.tf
@@ -4,30 +4,29 @@
 module "vpc" {
   source = "../../aws-tf-modules/module.cognito-userpool"
 
-  environment    = var.environment
   default_region = var.default_region
-
-  component_name = ""
+  environment    = var.environment
+  component_name = var.component_name
 
   user_pool_name             = var.user_pool_name
   is_username_case_sensitive = var.is_username_case_sensitive
   alias_attributes           = var.alias_attributes
   auto_verified_attributes   = var.auto_verified_attributes
 
-  mfa_configuration = var.mfa_configuration
+  mfa_configuration                = var.mfa_configuration
+  software_token_mfa_configuration = var.software_token_mfa_configuration
+  admin_create_user_config         = var.admin_create_user_config
+  email_configuration              = var.email_configuration
+  verification_message_template    = var.verification_message_template
 
-  admin_create_user_config = var.admin_create_user_config
-
-  email_configuration = var.email_configuration
-
-  schemas                       = var.schemas
-  string_schemas                = var.string_schemas
-  verification_message_template = var.verification_message_template
-  recovery_mechanisms           = var.recovery_mechanisms
+  schemas             = var.schemas
+  string_schemas      = var.string_schemas
+  recovery_mechanisms = var.recovery_mechanisms
 
   password_policy   = var.password_policy
   user_pool_add_ons = var.user_pool_add_ons
 
 
-  clients = var.clients
+  clients        = var.clients
+  route53_domain = var.route53_domain
 }
