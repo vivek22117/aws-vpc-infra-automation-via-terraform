096f0f0ce9263f8df9727e2945d45242e5ca94f7
vivek22117 <vivekmishra22117@gmail.com>
2022-06-26 20:01:30 +0530

    adding outputs for Cognito userpool

--- aws-tf-modules/module.cognito-userpool/output.tf
+++ aws-tf-modules/module.cognito-userpool/output.tf
@@ -1,29 +1,29 @@
 output "userpool_id" {
   description = "The id of the user pool"
-  value       = module.cognito_userpool.id
+  value       = var.enabled ? aws_cognito_user_pool.pool[0].id : null
 }
 
 output "userpool_arn" {
   description = "The ARN of the user pool"
-  value       = module.cognito_userpool.arn
+  value       = var.enabled ? aws_cognito_user_pool.pool[0].arn : null
 }
 
 output "userpool_endpoint" {
   description = "The endpoint name of the user pool. Example format: cognito-idp.REGION.amazonaws.com/xxxx_yyyyy"
-  value       = module.cognito_userpool.userpool_endpoint
+  value       = var.enabled ? aws_cognito_user_pool.pool[0].endpoint : null
 }
 
 output "creation_date" {
   description = "The date the user pool was created"
-  value       = module.cognito_userpool.creation_date
+  value       = var.enabled ? aws_cognito_user_pool.pool[0].creation_date : null
 }
 
 output "client_ids" {
   description = "The ids of the user pool clients"
-  value       = module.cognito_userpool.client_ids
+  value       = var.enabled ? aws_cognito_user_pool_client.client.*.id : null
 }
 
 output "client_ids_map" {
   description = "The ids map of the user pool clients"
-  value       = module.cognito_userpool.client_ids_map
+  value       = var.enabled ? { for k, v in aws_cognito_user_pool_client.client : v.name => v.id } : null
 }
