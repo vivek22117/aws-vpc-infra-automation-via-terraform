73b593d53d7c6cc0a6b3d10ebaf5f3aac1d577d5
vivek22117 <vivekmishra22117@gmail.com>
2022-06-26 20:35:39 +0530

    adding dependency for cognito domain config

--- aws-tf-modules/module.aws-route53/route_53.tf
+++ aws-tf-modules/module.aws-route53/route_53.tf
@@ -50,7 +50,7 @@ resource "aws_acm_certificate_validation" "cert" {
 resource "aws_route53_record" "cognito_web_1" {
 
   zone_id = data.aws_route53_zone.cloud_interview.zone_id
-  name    = "auth.cloud-interview.in"
+  name    = aws_cognito_user_pool_domain.user_pool_domain.domain
   type    = "A"
 
   alias {
@@ -61,17 +61,4 @@ resource "aws_route53_record" "cognito_web_1" {
   }
 }
 
-resource "aws_route53_record" "cognito_web_2" {
-
-  zone_id = data.aws_route53_zone.cloud_interview.zone_id
-  name    = "www.auth.cloud-interview.in.in"
-  type    = "A"
-
-  alias {
-    name = aws_cognito_user_pool_domain.user_pool_domain[0].cloudfront_distribution_arn
-    # This zone_id is fixed
-    zone_id                = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
-}
 
