43fe05d74e4d9235c715900c7693a822ca24e2d2
vivek22117 <vivekmishra22117@gmail.com>
2022-06-26 20:25:10 +0530

    adding dependency for cognito domain config

--- aws-tf-modules/module.aws-route53/domain.tf
+++ aws-tf-modules/module.aws-route53/domain.tf
@@ -6,7 +6,7 @@ resource "aws_cognito_user_pool_domain" "user_pool_domain" {
   user_pool_id    = data.terraform_remote_state.cognito_userpool.outputs.userpool_id
 
   depends_on = [
-    aws_route53_record.cognito_web_1,
-    aws_route53_record.cognito_web_2
+    aws_route53_record.cert_validation,
+    aws_acm_certificate_validation.cert
   ]
 }
