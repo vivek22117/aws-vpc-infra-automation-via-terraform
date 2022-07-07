77f12a963359e5b12223946c20e3ea94d064c7f6
vivek22117 <vivekmishra22117@gmail.com>
2022-07-07 16:07:39 +0530

    adding variable for inspection VPC

---
+++ aws-tf-modules/moudle.aws-inpection-vpc/variables.tf
@@ -0,0 +1,27 @@
+######################################################################
+# Global variables for VPC, Subnet, Routes and Bastion Host          #
+######################################################################
+variable "default_region" {
+  type        = string
+  description = "AWS region to deploy resources"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "CIDR range for vpc"
+}
+
+variable "instance_tenancy" {
+  type        = string
+  description = "Type of instance tenancy required default/dedicated"
+}
+
+variable "enable_dns" {
+  type        = string
+  description = "To use private DNS within the VPC"
+}
+
+variable "support_dns" {
+  type        = string
+  description = "To use private DNS support within the VPC"
+}
