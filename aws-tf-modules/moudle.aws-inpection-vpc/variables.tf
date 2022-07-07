87ad01bf500833ac3d68e2b1501a93970fd71d5e
vivek22117 <vivekmishra22117@gmail.com>
2022-07-07 16:09:03 +0530

    updating variables

--- aws-tf-modules/moudle.aws-inpection-vpc/variables.tf
+++ aws-tf-modules/moudle.aws-inpection-vpc/variables.tf
@@ -25,3 +25,43 @@ variable "support_dns" {
   type        = string
   description = "To use private DNS support within the VPC"
 }
+
+######################################################
+# Local variables defined                            #
+######################################################
+variable "team" {
+  type        = string
+  description = "Owner team for this application infrastructure"
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner of the product"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment to be used"
+}
+
+variable "isMonitoring" {
+  type        = bool
+  description = "Monitoring is enabled or disabled for the resources creating"
+}
+
+variable "project" {
+  type        = string
+  description = "Monitoring is enabled or disabled for the resources creating"
+}
+
+
+#####=============Local variables===============#####
+locals {
+  common_tags = {
+    owner       = var.owner
+    team        = var.team
+    environment = var.environment
+    monitoring  = var.isMonitoring
+    Project     = var.project
+  }
+}
