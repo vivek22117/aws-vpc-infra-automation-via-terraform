bb2ebecd982557dbe19f819193eb6300115ecad1
vivek22117 <vivekmishra22117@gmail.com>
2022-07-08 18:17:10 +0530

    adding public subnet for inspection VPC

--- aws-tf-modules/moudle.aws-inpection-vpc/vpc.tf
+++ aws-tf-modules/moudle.aws-inpection-vpc/vpc.tf
@@ -1,3 +1,12 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+  list_of_azs = data.aws_availability_zones.available.names
+
+  total_azs = length(data.aws_availability_zones.available.names)
+  used_azs  = local.total_azs > 3 ? 3 : local.total_azs
+}
+
 #################################################
 #       VPC Configuration                       #
 #################################################
@@ -8,20 +17,20 @@ resource "aws_vpc" "inspection_vpc" {
   enable_dns_support   = var.support_dns
 
 
-  tags = merge(local.common_tags, tomap({ "Name" = "iinspection-vpc-${var.environment}-${var.cidr_block}" }))
+  tags = merge(local.common_tags, tomap({ "Name" = "inspection-vpc-${var.environment}-${var.cidr_block}" }))
 }
 
-//resource "aws_subnet" "inspection_vpc_public_subnet" {
-//  count                   = length(data.aws_availability_zones.available.names)
-//  map_public_ip_on_launch = true
-//  vpc_id                  = aws_vpc.inspection_vpc.id
-//  availability_zone       = data.aws_availability_zones.available.names[count.index]
-//  cidr_block              = cidrsubnet(local.inspection_vpc_cidr, 8, 10 + count.index)
-//  depends_on              = [aws_internet_gateway.inspection_vpc_igw]
-//  tags = {
-//    Name = "inspection-vpc/${data.aws_availability_zones.available.names[count.index]}/public-subnet"
-//  }
-//}
+resource "aws_subnet" "inspection_vpc_public_subnet" {
+  count = length(data.aws_availability_zones.available.names)
+
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.inspection_vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 10 + count.index)
+
+  tags = merge(local.common_tags, tomap({ "Name" = "inspection-vpc/${data.aws_availability_zones.available.names[count.index]}/public-subnet" }))
+}
+
 //
 //resource "aws_subnet" "inspection_vpc_firewall_subnet" {
 //  count                   = length(data.aws_availability_zones.available.names)
