b892d2923ef0ae9d3d98d2af79eb174451ee13d9
vivek22117 <vivekmishra22117@gmail.com>
2022-07-17 20:05:20 +0530

    updating variables

--- aws-tf-modules/module.vpc-client-vpn/variables.tf
+++ aws-tf-modules/module.vpc-client-vpn/variables.tf
@@ -34,6 +34,33 @@ variable "project" {
   description = "Monitoring is enabled or disabled for the resources creating"
 }
 
+#######===========================VPN Client Variables=============================#######
+variable "client_cidr_block" {
+  description = "AWS VPN client cidr block"
+  type        = string
+}
+variable "split_tunnel" {
+  description = "Split tunnel traffic"
+  type        = bool
+}
+variable "vpn_inactive_period" {
+  description = "VPN inactive period in seconds"
+  type        = number
+}
+variable "session_timeout_hours" {
+  description = "Session timeout hours"
+  type        = number
+}
+variable "logs_retention_in_days" {
+  description = "VPN client list!?"
+  type        = number
+}
+
+variable "aws-vpn-client-list" {
+  description = "VPN client list of users ?"
+  type        = list(string)
+}
+
 #####=============Local variables===============#####
 locals {
   common_tags = {
