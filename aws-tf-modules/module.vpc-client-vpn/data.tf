885b4cb97f3b665ede71d09c4138e5941fe0dc8d
vivek22117 <vivekmishra22117@gmail.com>
2022-07-17 20:05:01 +0530

    adding data resource to read S3 state file

---
+++ aws-tf-modules/module.vpc-client-vpn/data.tf
@@ -0,0 +1,25 @@
+####################################################
+#             Reading VPC TF SateFile              #
+####################################################
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key    = "state/${var.environment}/vpc/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key    = "state/${var.environment}/s3-buckets/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+# used for accessing Account ID and ARN
+data "aws_caller_identity" "current" {}
