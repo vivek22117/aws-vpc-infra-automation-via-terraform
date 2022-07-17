b31bd617c1118b02b7fff4c6779cf10d1af3e945
vivek22117 <vivekmishra22117@gmail.com>
2022-07-17 20:32:09 +0530

    updating route table for traffic flow

--- aws-tf-modules/moudle.aws-inpection-vpc/route-table.tf
+++ aws-tf-modules/moudle.aws-inpection-vpc/route-table.tf
@@ -1,5 +1,8 @@
+##############################################################################
+#               Inspection VPC TGW RouteTable & its Association              #
+##############################################################################
 resource "aws_route_table" "inspection_vpc_tgw_subnet_route_table" {
-  count = length(data.aws_availability_zones.available.names)
+  count = local.used_azs
 
   vpc_id = aws_vpc.inspection_vpc.id
   route {
@@ -13,15 +16,18 @@ resource "aws_route_table" "inspection_vpc_tgw_subnet_route_table" {
 }
 
 resource "aws_route_table_association" "inspection_vpc_tgw_subnet_route_table_association" {
-  count = length(data.aws_availability_zones.available.names)
+  count = local.used_azs
 
   route_table_id = aws_route_table.inspection_vpc_tgw_subnet_route_table[count.index].id
   subnet_id      = aws_subnet.inspection_vpc_tgw_subnet[count.index].id
 }
 
-
+###################################################################################
+#               Inspection VPC Firewall RouteTable & its Association              #
+###################################################################################
 resource "aws_route_table" "inspection_vpc_firewall_subnet_route_table" {
-  count  = length(data.aws_availability_zones.available.names)
+  count = local.used_azs
+
   vpc_id = aws_vpc.inspection_vpc.id
   route {
     cidr_block         = "10.0.0.0/8"
@@ -34,4 +40,38 @@ resource "aws_route_table" "inspection_vpc_firewall_subnet_route_table" {
   tags = {
     Name = "inspection-vpc/${data.aws_availability_zones.available.names[count.index]}/firewall-subnet-route-table"
   }
+}
+
+resource "aws_route_table_association" "inspection_vpc_firewall_subnet_route_table_association" {
+  count = local.used_azs
+
+  route_table_id = aws_route_table.inspection_vpc_firewall_subnet_route_table[count.index].id
+  subnet_id      = aws_subnet.inspection_vpc_firewall_subnet[count.index].id
+}
+
+#################################################################################
+#               Inspection VPC Public RouteTable & its Association              #
+#################################################################################
+resource "aws_route_table" "inspection_vpc_public_subnet_route_table" {
+  count = local.used_azs
+
+  vpc_id = aws_vpc.inspection_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.inspection_vpc_igw.id
+  }
+  route {
+    cidr_block      = "10.0.0.0/8"
+    vpc_endpoint_id = element([for ss in tolist(aws_networkfirewall_firewall.inspection_vpc_nt_firewall.firewall_status.sync_states) : ss.attachment[0].endpoint_id if ss.attachment[0].subnet_id == aws_subnet.inspection_vpc_firewall_subnet[count.index].id], 0)
+  }
+  tags = {
+    Name = "inspection-vpc/${data.aws_availability_zones.available.names[count.index]}/public-subnet-route-table"
+  }
+}
+
+resource "aws_route_table_association" "inspection_vpc_public_subnet_route_table_association" {
+  count = local.used_azs
+
+  route_table_id = aws_route_table.inspection_vpc_public_subnet_route_table[count.index].id
+  subnet_id      = aws_subnet.inspection_vpc_public_subnet[count.index].id
 }
