b6c7a3a3a4fd41ed35ab204461b186f4b7fd1469
vivek22117 <vivekmishra22117@gmail.com>
2022-07-20 10:30:31 +0530

    emoving unused resources

--- aws-tf-modules/moudle.aws-inpection-vpc/transit-gateway.tf
+++ aws-tf-modules/moudle.aws-inpection-vpc/transit-gateway.tf
@@ -60,30 +60,3 @@ resource "aws_ec2_transit_gateway_route_table_association" "inspection_vpc_tgw_a
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.inspection_vpc_route_table.id
 }
 
-//
-//resource "aws_ec2_transit_gateway_route" "spoke_route_table_default_route" {
-//  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.inspection_vpc_tgw_attachment.id
-//  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke_route_table.id
-//  destination_cidr_block         = "0.0.0.0/0"
-//
-//}
-//
-//resource "aws_ec2_transit_gateway_route_table_association" "inspection_vpc_tgw_attachment_rt_association" {
-//  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.inspection_vpc_tgw_attachment.id
-//  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.inspection_route_table.id
-//}
-//
-//resource "aws_ec2_transit_gateway_route_table_propagation" "inspection_route_table_propagate_spoke_vpc_a" {
-//  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.spoke_vpc_a_tgw_attachment.id
-//  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.inspection_route_table.id
-//}
-//
-//resource "aws_ec2_transit_gateway_route_table_propagation" "inspection_route_table_propagate_spoke_vpc_b" {
-//  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.spoke_vpc_b_tgw_attachment.id
-//  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.inspection_route_table.id
-//}
-//
-//resource "aws_ec2_transit_gateway_route_table_propagation" "spoke_route_table_propagate_inspection_vpc" {
-//  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.inspection_vpc_tgw_attachment.id
-//  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke_route_table.id
-//}
