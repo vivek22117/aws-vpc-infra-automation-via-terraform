f8d72039a9408ebe6c4144ee649cc0f24b370584
vivek22117 <vivekmishra22117@gmail.com>
2022-07-23 17:15:11 +0530

    adding necessary variables

--- deployment/insepction-vpc/main.tf
+++ deployment/insepction-vpc/main.tf
@@ -10,4 +10,15 @@ module "inspection_vpc" {
   owner          = var.owner
   project        = var.project
   team           = var.team
+
+
+  cidr_block                        = var.cidr_block
+  create_network_firewall           = var.create_network_firewall
+  delete_protection                 = var.delete_protection
+  enable_dns                        = var.enable_dns
+  enable_nat_gateway                = var.enable_nat_gateway
+  firewall_policy_change_protection = var.firewall_policy_change_protection
+  instance_tenancy                  = var.instance_tenancy
+  subnet_change_protection          = var.subnet_change_protection
+  support_dns                       = var.support_dns
 }
