28fcea5775912e40f6bbd861a49dc42f1c7323f3
vivek22117 <vivekmishra22117@gmail.com>
2022-08-01 10:59:18 +0530

    adding necessary variables

--- deployment/vpc/main.tf
+++ deployment/vpc/main.tf
@@ -7,15 +7,17 @@ module "vpc" {
   environment    = var.environment
   default_region = var.default_region
 
-  cidr_block            = var.cidr_block
-  private_azs_with_cidr = var.private_azs_with_cidr
-  public_azs_with_cidr  = var.public_azs_with_cidr
-  db_azs_with_cidr      = var.db_azs_with_cidr
+  cidr_block                 = var.cidr_block
+  private_azs_with_cidr      = var.private_azs_with_cidr
+  public_azs_with_cidr       = var.public_azs_with_cidr
+  db_azs_with_cidr           = var.db_azs_with_cidr
   vault_consul_azs_with_cidr = var.vault_consul_azs_with_cidr
-  instance_tenancy      = var.instance_tenancy
-  enable_dns            = var.enable_dns
-  support_dns           = var.support_dns
-  enable_nat_gateway    = var.enable_nat_gateway
+  tgw_azs_with_cidr          = var.tgw_azs_with_cidr
+
+  instance_tenancy   = var.instance_tenancy
+  enable_dns         = var.enable_dns
+  support_dns        = var.support_dns
+  enable_nat_gateway = var.enable_nat_gateway
 
   team         = var.team
   owner        = var.owner
